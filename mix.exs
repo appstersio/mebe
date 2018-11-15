76344241ff4a38c021f9cfd018d1043fc2abcdf1
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2018-11-15 22:00:54 +0200

    Update to eex_html 0.2.0, allowing deleting of useless raw() calls

--- mix.exs
+++ mix.exs
@@ -23,8 +23,9 @@ defmodule Mebe2.MixProject do
   defp deps do
     [
       {:earmark, "~> 1.2.5"},
-      {:raxx, "~> 0.17.1"},
+      {:raxx, "~> 0.17.2"},
       {:raxx_static, "~> 0.7.0"},
+      {:eex_html, "~> 0.2.0"},
       {:ace, "~> 0.18.0"},
       {:calendar, "~> 0.17.4"},
       {:ex2ms, "~> 1.5.0"},
